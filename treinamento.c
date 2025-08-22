d800f7b2907505eaccd85a85aca7e696f2337077
Taís Pereira <202503945152@alunos.estacio.br>
2025-08-22 18:19:22 -0300

    Create treinamento.c

---
+++ treinamento.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+    int main(){
+        int idade, matricula;
+        float altura;
+        char nome[50];
+       
+        printf("Digite sua idade:\n");
+        scanf("%d", &idade);
+
+        printf("Digite sua altura: \n");
+        scanf("%f", &altura);
+
+    print("Digite o seu nome: \n");
+scanf("%s", &nome);
+
+Printf("Digite sua matricula: \n");
+scanf("%d", &matricula);
+
+printf("nome do aluno: %s - matricula:%d", nome, matricula);
+printf("Idade %d - Altura: %f", idade, altura);
+
+return 0;
