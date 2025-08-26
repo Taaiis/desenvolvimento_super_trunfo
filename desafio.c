41d88e7d7c62c66e69abba9d29ae5147f68ca185
Taís Pereira <202503945152@alunos.estacio.br>
2025-08-26 07:32:09 -0300

    Fix syntax errors and remove treinamento.c
    Removed treinamento.c file and fixed syntax errors in desafio.c.

--- desafio.c
+++ desafio.c
@@ -1,12 +1,1 @@
-include <stdio.h>
-int main (){
-   char estado [A a H], código da carta [], nome da cidade;
-   int população, números de pontos turísticos;
-   float área da cidade, PIB;
-
-printf ("Digite o estado de sua carta" (A a H):\n)
-  scanf (%s, %estado)
-
-
-
-
+git rm treinamento.c
